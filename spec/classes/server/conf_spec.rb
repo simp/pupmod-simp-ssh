de5d8784165699558818c58e514c3196b4642117
Chris Tessmer <chris.tessmer@onyxpoint.com>
2016-05-13 22:13:59 -0400

    (SIMP-1054) Update static assets to fix spec tests
    Before this patch, this module would fail its spec tests when
    `STRICT_VARIABLES=yes` is set (as it is in Travis CI tests).
    This was caused by the presence of the unset (and unused) variable
    "spec_title" in the modules' fixture hieradata/hiera.yaml.
    
    This commit fixes the issue by removing "spec_title" from
    each module's hieradata/hiera.yaml.
    
    SIMP-1054 #comment Fixed pupmod-simp-ssh
    SIMP-1097 #close Fixed spec failures when `STRICT_VARIABLES=yes`
    
    Change-Id: I835a41178af3f3b816ccd36f86e02e49a203e2b8

--- spec/classes/server/conf_spec.rb
+++ spec/classes/server/conf_spec.rb
@@ -4,48 +4,12 @@ describe 'ssh::server::conf' do
 
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
-      let(:facts) do
-        facts
-      end
+      let(:facts) { facts }
 
-      context "on #{os}" do
-        shared_examples_for "a fact set conf" do
-          it { is_expected.to create_class('ssh::server::conf') }
-          it { is_expected.to compile.with_all_deps }
-          it { is_expected.to create_file('/etc/ssh/sshd_config') }
-          it { is_expected.to create_file('/etc/ssh/local_keys') }
-        end
-
-        describe "RHEL 6" do
-          it_behaves_like "a fact set conf"
-
-          let(:facts) {{
-            :fqdn => 'spec.test',
-            :uid_min => '500',
-            :grub_version => '0.97',
-            :hardwaremodel => 'x86_64',
-            :operatingsystem => 'RedHat',
-            :operatingsystemmajrelease => '6',
-            :operatingsystemmajrelease => '6',
-            :interfaces => 'eth0,lo'
-          }}
-        end
-
-        describe "RHEL 7" do
-          it_behaves_like "a fact set conf"
-
-          let(:facts) {{
-            :fqdn => 'spec.test',
-            :uid_min => '500',
-            :grub_version => '2.0.2~beta2',
-            :hardwaremodel => 'x86_64',
-            :operatingsystem => 'RedHat',
-            :operatingsystemmajrelease => '7',
-            :operatingsystemmajrelease => '7',
-            :interfaces => 'eth0,lo'
-          }}
-        end
-      end
+      it { is_expected.to create_class('ssh::server::conf') }
+      it { is_expected.to compile.with_all_deps }
+      it { is_expected.to create_file('/etc/ssh/sshd_config') }
+      it { is_expected.to create_file('/etc/ssh/local_keys') }
     end
   end
 end
