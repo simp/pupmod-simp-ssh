333fb69cd0be4868945daea0e20e8c5ee7e07ec7
Nick Miller <nick.miller@onyxpoint.com>
2016-06-13 15:22:41 -0400

    (SIMP-1187) Update iptables and ldap to use globals
    Updates to acceptance tests and a few global catalysts.
    
    SIMP-1187 #close
    
    Change-Id: I08217ec57410585daba874d2eefc51b3c0d4a689

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -59,25 +59,26 @@ describe 'ssh::server' do
 
           if (['RedHat', 'CentOS'].include?(facts[:operatingsystem]))
             if (facts[:operatingsystemmajrelease].to_s >= '7')
+
               context "with fips enabled" do
                 let(:facts) { facts.merge( { :fips_enabled => true, :openssh_version => '6.6' } ) }
                 it { is_expected.to contain_sshd_config('Ciphers').with_value(
                      ['aes256-gcm@openssh.com',
-                     'aes128-gcm@openssh.com',
-                     'aes256-cbc',
-                     'aes192-cbc',
-                     'aes128-cbc'])
+                      'aes128-gcm@openssh.com',
+                      'aes256-cbc',
+                      'aes192-cbc',
+                      'aes128-cbc'])
                 }
                 it { is_expected.to contain_sshd_config('MACs').with_value(
                      ['hmac-sha2-256',
-                     'hmac-sha1'])
+                      'hmac-sha1'])
                 }
                 context "with openssh_version 6.6" do
                   it { is_expected.to contain_sshd_config('KexAlgorithms').with_value(
                        ['ecdh-sha2-nistp521',
-                       'ecdh-sha2-nistp384',
-                       'ecdh-sha2-nistp256',
-                       'diffie-hellman-group-exchange-sha256'])
+                        'ecdh-sha2-nistp384',
+                        'ecdh-sha2-nistp256',
+                        'diffie-hellman-group-exchange-sha256'])
                   }
                 end
                 context "with openssh_version 5.6" do
@@ -89,46 +90,49 @@ describe 'ssh::server' do
                 let(:facts) { facts.merge( { :fips_enabled => false, :openssh_version => '6.6' } ) }
                 it { is_expected.to contain_sshd_config('Ciphers').with_value(
                      ['aes256-gcm@openssh.com',
-                     'aes128-gcm@openssh.com',
-                     'aes256-cbc',
-                     'aes192-cbc',
-                     'aes128-cbc'])
+                      'aes128-gcm@openssh.com',
+                      'aes256-cbc',
+                      'aes192-cbc',
+                      'aes128-cbc'])
                 }
                 it { is_expected.to contain_sshd_config('MACs').with_value(
                      ['hmac-sha2-512-etm@openssh.com',
-                     'hmac-sha2-256-etm@openssh.com',
-                     'hmac-sha2-512',
-                     'hmac-sha2-256'])
+                      'hmac-sha2-256-etm@openssh.com',
+                      'hmac-sha2-512',
+                      'hmac-sha2-256'])
                 }
                 context "with openssh_version 6.6" do
                   it { is_expected.to contain_sshd_config('KexAlgorithms').with_value(
                        ['curve25519-sha256@libssh.org',
-                       'ecdh-sha2-nistp521',
-                       'ecdh-sha2-nistp384',
-                       'ecdh-sha2-nistp256',
-                       'diffie-hellman-group-exchange-sha256'])
+                        'ecdh-sha2-nistp521',
+                        'ecdh-sha2-nistp384',
+                        'ecdh-sha2-nistp256',
+                        'diffie-hellman-group-exchange-sha256'])
                   }
                 end
                 context "with openssh_version 6.4" do
                   let(:facts) { facts.merge( { :openssh_version => '6.4' } ) }
                   it { is_expected.to contain_sshd_config('KexAlgorithms').with_value(
                        ['ecdh-sha2-nistp521',
-                       'ecdh-sha2-nistp384',
-                       'ecdh-sha2-nistp256',
-                       'diffie-hellman-group-exchange-sha256'])
+                        'ecdh-sha2-nistp384',
+                        'ecdh-sha2-nistp256',
+                        'diffie-hellman-group-exchange-sha256'])
                   }
                 end
                 context "with openssh_version 5.6" do
                   let(:facts) { facts.merge( { :openssh_version => '5.6' } ) }
                   it { is_expected.to_not contain_sshd_config('KexAlgorithms') }
                 end
+
+                it { is_expected.to_not contain_package('openssh-ldap').with_ensure('latest') }
+
               end
             # Not EL 7+ OS
             else
               it { is_expected.to contain_sshd_config('Ciphers').with_value(
                    ['aes256-cbc',
-                   'aes192-cbc',
-                   'aes128-cbc'])
+                    'aes192-cbc',
+                    'aes128-cbc'])
               }
               it { is_expected.to contain_sshd_config('MACs').with_value(
                    ['hmac-sha1'])
@@ -144,16 +148,14 @@ describe 'ssh::server' do
                 it { is_expected.to_not contain_sshd_config('KexAlgorithms') }
               end
             end
-
-            if (facts[:operatingsystemrelease].to_s < '6.7')
-              it { is_expected.to contain_package('openssh-ldap').with_ensure('latest') }
-            else
-              it { is_expected.to_not contain_package('openssh-ldap').with_ensure('latest') }
-            end
           end
         end
 
-        context "with enable_fallback_ciphers=false" do
+        if (facts[:operatingsystemrelease].to_s < '6.7')
+          it { is_expected.to contain_package('openssh-ldap').with_ensure('latest') }
+        end
+
+        context "with enable_fallback_ciphers => false" do
           let(:pre_condition){
             "class{'ssh::server::conf': enable_fallback_ciphers => false }"
           }
@@ -161,16 +163,25 @@ describe 'ssh::server' do
           if (['RedHat', 'CentOS'].include?(facts[:operatingsystem]))
             if (facts[:operatingsystemmajrelease].to_s >= '7')
               it { is_expected.to contain_sshd_config('Ciphers').with_value(
-                   ['aes256-gcm@openssh.com', 'aes128-gcm@openssh.com'])
+                ['aes256-gcm@openssh.com',
+                 'aes128-gcm@openssh.com'])
               }
             else
               it { is_expected.to contain_sshd_config('Ciphers').with_value(
-                   ['aes256-cbc',
-                   'aes192-cbc',
-                   'aes128-cbc'])
+                ['aes256-cbc',
+                 'aes192-cbc',
+                 'aes128-cbc'])
               }
             end
           end
+        end
+
+        context "with use_ldap => false" do
+          let(:pre_condition){
+            "class{'ssh::server::conf': use_ldap => false }"
+          }
+          it_behaves_like "an ssh server"
+          it { is_expected.to_not contain_package('openssh-ldap').with_ensure('latest') }
         end
 
         context "with a non-standard ssh port" do
