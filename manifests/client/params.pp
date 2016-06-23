7c3b44533773ede5d47744d86338eaf592b91150
nick-markowski <nmarkowski@keywcorp.com>
2016-06-23 13:01:41 +0000

    (SIMP-1177) Include haveged for entropy generation
    SIMP-1177 #comment ssh updated
    Change-Id: Ibd5694e37ed4578778100020b44285f96e0e333f

--- manifests/client/params.pp
+++ manifests/client/params.pp
@@ -7,6 +7,7 @@
 # * Trevor Vaughan <mailto:tvaughan@onyxpoint.com>
 #
 class ssh::client::params {
+
   # These are all that are supported on RHEL6
   $_fallback_macs = [ 'hmac-sha1' ]
   $_fallback_ciphers = [
