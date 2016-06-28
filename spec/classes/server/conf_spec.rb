9bf6ef3172dfad634860ed8437e47fa08a7bc0d9
nick-markowski <nmarkowski@keywcorp.com>
2016-06-28 18:44:24 +0000

    (SIMP-1177) Include haveged for entropy generation
    SIMP-1177 #comment ssh updated
    Change-Id: Ibd5694e37ed4578778100020b44285f96e0e333f

--- spec/classes/server/conf_spec.rb
+++ spec/classes/server/conf_spec.rb
@@ -4,18 +4,34 @@ describe 'ssh::server::conf' do
 
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
-      let(:facts) { facts }
-
-      let(:pre_condition){ 'include "::ssh"' }
       let(:facts) do
         facts
       end
 
-      let(:facts) { facts.merge( { :openssh_version => '6.6' } ) }
-      it { is_expected.to create_class('ssh::server::conf') }
-      it { is_expected.to compile.with_all_deps }
-      it { is_expected.to create_file('/etc/ssh/sshd_config') }
-      it { is_expected.to create_file('/etc/ssh/local_keys') }
+      context "on os #{os}" do
+        let(:facts) { facts.merge( { :openssh_version => '6.6' } ) }
+
+        context 'with default parameters' do
+          let(:pre_condition){ 'include "::ssh"' }
+          it { is_expected.to create_class('ssh::server::conf') }
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to create_file('/etc/ssh/sshd_config') }
+          it { is_expected.to create_file('/etc/ssh/local_keys') }
+          it { is_expected.to contain_class('haveged') }
+        end
+        context 'with use_haveged => false' do
+          let(:params) {{:use_haveged => false}}
+          it { is_expected.to_not contain_class('haveged') }
+        end
+        context 'with invalid input' do
+          let(:params) {{:use_haveged => 'invalid_input'}}
+          it 'with use_haveged as a string' do
+            expect {
+              is_expected.to compile
+            }.to raise_error(RSpec::Expectations::ExpectationNotMetError,/invalid_input" is not a boolean/)
+          end
+        end
+      end
     end
   end
 end
