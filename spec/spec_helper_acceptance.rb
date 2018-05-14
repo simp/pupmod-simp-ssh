8d3db41d8dc7856d9d6b2070a40be3cf0d14d9f3
jeannegreulich <jeanne.greulich@onyxpoint.com>
2018-05-14 17:27:34 -0400

    (SIMP-4688) SSH Stig updates and inspec tests (#74)
    - added additional params to ssh server conf to make sure
      settings which are default would show up in the config
      file.  This would allow compliance testing to pass with
      out errors.  There should be no change to functionality.
    - added compliance test to install compliance module settings
      and then run inspec tests.
    - change ssh::server::conf to 'delayed' by default
    
    SIMP-4789 #close
    SIMP-4868 #close
    SIMP-4869 #comment  Added tests but also need inspec module updates
    SIMP-4895 #close

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,10 +4,6 @@ require 'yaml'
 require 'simp/beaker_helpers'
 include Simp::BeakerHelpers
 
-require 'pry' if ENV['PRY'] == 'yes'
-$LOAD_PATH.unshift(File.expand_path('../acceptance/support',__FILE__))
-
-
 unless ENV['BEAKER_provision'] == 'no'
   hosts.each do |host|
     # Install Puppet
@@ -16,6 +12,9 @@ unless ENV['BEAKER_provision'] == 'no'
     else
       install_puppet
     end
+    # Install git, it's a dependency for inspec profiles
+    # Found this when experiencing https://github.com/chef/inspec/issues/1270
+    install_package(host, 'git')
   end
 end
 
@@ -37,18 +36,6 @@ RSpec.configure do |c|
       rescue ArgumentError =>e
         server = only_host_with_role(hosts, 'default')
       end
-
-      pluginsync_manifest =<<-PLUGINSYNC_MANIFEST
-        file { $::settings::libdir:
-              ensure  => directory,
-              source  => 'puppet:///plugins',
-              recurse => true,
-              purge   => true,
-              backup  => false,
-              noop    => false
-            }
-      PLUGINSYNC_MANIFEST
-      apply_manifest_on(hosts, pluginsync_manifest)
 
       # Generate and install PKI certificates on each SUT
       Dir.mktmpdir do |cert_dir|
