3593667a41bfa1570e9cd6d042476a2aafccbe6a
jeannegreulich <jeanne.greulich@onyxpoint.com>
2021-03-03 08:40:35 -0500

    (SIMP-9412) SSH iselinux_port errors (#133)
    - Running selinux_port was failing when the voxpupuli selinux
      modules was not included. selinux_port requires the packages
      installed by the module.  Verified that including the module
      would not do anything by default except install the packages.
    - Added memory to the EL8 test nodes.  Tests were consistently getting an
      out of memory error when selinux_port was called.  semanage,
      used by the selinux_port resource, allocates a lot of memory
      when loading the selinux policies it needs.
    
    SIMP-9412 #close
    
    Co-authored-by: jgreu <jeagreu@gmail.com>

--- manifests/server/conf.pp
+++ manifests/server/conf.pp
@@ -539,10 +539,12 @@ class ssh::server::conf (
         simplib::assert_optional_dependency($module_name, 'simp/selinux')
         simplib::assert_optional_dependency($module_name, 'simp/vox_selinux')
 
-        include selinux::install
+        include vox_selinux
+
       }
       else {
         simplib::assert_optional_dependency($module_name, 'puppet/selinux')
+        include selinux
       }
 
       selinux_port { "tcp_${sel_port}-${sel_port}":
