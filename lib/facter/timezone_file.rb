d2b18672f1032c2b1409e93c013d40c2cd8eff84
Steven Pritchard <steve@sicura.us>
2023-12-11 11:55:34 -0600

    Minor fixes for rspec and rubocop

--- lib/facter/timezone_file.rb
+++ lib/facter/timezone_file.rb
@@ -2,17 +2,17 @@
 #
 # Return the path for the timezone file the system is using
 #
-Facter.add("timezone_file") do
+Facter.add(:timezone_file) do
+  confine kernel: :linux
   setcode do
     # If /etc/localtime doesn't exist, use the appropriate file in /usr/share/zoneinfo
-    timezone_file = ''
-    if File.exist?('/etc/localtime')
-      timezone_file = '/etc/localtime'
-    elsif File.exist?("/usr/share/zoneinfo/#{Facter.value('timezone')}")
-      timezone_file = "/usr/share/zoneinfo/#{Facter.value('timezone')}"
-    else
-      timezone_file = '/usr/share/zoneinfo/UTC'
-    end
+    timezone_file = if File.exist?('/etc/localtime')
+                      '/etc/localtime'
+                    elsif File.exist?("/usr/share/zoneinfo/#{Facter.value('timezone')}")
+                      "/usr/share/zoneinfo/#{Facter.value('timezone')}"
+                    else
+                      '/usr/share/zoneinfo/UTC'
+                    end
     timezone_file
   end
 end
