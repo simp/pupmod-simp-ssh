f7d8a527ada7cbeae35c4f75e8f5ae1b8c5502a0
Mike Riddle <michael.riddle@onyxpoint.com>
2023-12-11 11:00:57 -0500

    Accounting for if '/usr/share/zoneinfo/#{Facter.value('timezone')}' doesn't exist

--- lib/facter/timezone_file.rb
+++ lib/facter/timezone_file.rb
@@ -5,7 +5,14 @@
 Facter.add("timezone_file") do
   setcode do
     # If /etc/localtime doesn't exist, use the appropriate file in /usr/share/zoneinfo
-    timezone_file = File.exist?('/etc/localtime') ? '/etc/localtime' : "/usr/share/zoneinfo/#{Facter.value('timezone')}"
+    timezone_file = ''
+    if File.exist?('/etc/localtime')
+      timezone_file = '/etc/localtime'
+    elsif File.exist?("/usr/share/zoneinfo/#{Facter.value('timezone')}")
+      timezone_file = "/usr/share/zoneinfo/#{Facter.value('timezone')}"
+    else
+      timezone_file = '/usr/share/zoneinfo/UTC'
+    end
     timezone_file
   end
 end
